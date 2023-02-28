6887290d0d010f95245bc38de840a7b7dd8a5ab1
oimirza <123174281+oimirza@users.noreply.github.com>
2023-02-28 20:57:46 +0800

    Update long_track_python_titanic_dataset.py

--- long_track_python_titanic_dataset.py
+++ long_track_python_titanic_dataset.py
@@ -65,37 +65,4 @@ plt.title('Number of Survivors vs Non-Survivors by Gender')
 plt.ylabel('Number of Passengers')
 plt.show()
 
-!pip install sweetviz
 
-import sweetviz as sv
-
-advert_report = sv.analyze(df)
-
-advert_report.show_html('titanic.html')
-advert_report.show_notebook()
-
-from sklearn.model_selection import train_test_split
-
-X = df.drop('Survived', axis=1)
-y = df['Survived']
-
-X.head()
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-
-from sklearn.linear_model import LogisticRegression
-lm1= LogisticRegression()
-lm1.fit(X_train, y_train)
-
-
-ypred=lm1.predict(X_test)
-ypred
-
-
-from sklearn.metrics import accuracy_score
-accuracy_score(y_test, ypred)
-
-from sklearn.metrics import confusion_matrix
-confusion_matrix = confusion_matrix(y_test, ypred)
-print(confusion_matrix)
